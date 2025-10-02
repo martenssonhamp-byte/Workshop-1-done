938c28d9431859efd9e2768707bf77d58e0445aa
martenssonhamp-byte <martensson.hamp@gmail.com>
2025-10-02 10:37:47 +0200

    alot of design work DEL-A-2 done

--- Report-company.py
+++ Report-company.py
@@ -9,8 +9,23 @@ report = ""
 
 #1 Last updated report & company name
 report += "\n" + "Company Name: "  + data["company"] + "\n"
-report += "\n" + "LAST UPDATED:" "\n" + data["last_updated"] + "\n\n"
-    
+report += "\n" + "LAST UPDATED:" "\n" + data["last_updated"] + "\n"
+
+#2 List all devices with warning
+report += "\n" "ATTENTION! Devices with warnings" "\n"
+report += "--------------------------------------------------------------------------------------------" "\n"
+for location in data["locations"]:
+   for device in location["devices"]:
+      if device["status"] != "online" and device["status"] != "offline":
+         report += device["hostname"].ljust (12) + " | " + device["status"] + " | " + device["ip_address"].ljust (14) + " | " + device["type"].ljust (12) + " | " + location["site"] + "\n"
+# List all devices with Offline
+report += "\n" "ATTENTION! Devices with status offline!" "\n"
+report += "--------------------------------------------------------------------------------------------" "\n"
+for location in data["locations"]:
+   for device in location ["devices"]:
+      if device["status"] != "online" and device["status"] != "warning":
+        report += device["hostname"].ljust (12) + " | " + device["status"] + " | " + device["ip_address"].ljust (15) + " | " + device ["type"].ljust (12) + " | " + location["site"] + "\n"
+        
 
 # loop through the location list 
 for location in data["locations"]:
