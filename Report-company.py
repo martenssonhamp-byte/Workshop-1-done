7197fe6417fd986d0a966e6b5618587367a418df
martenssonhamp-byte <martensson.hamp@gmail.com>
2025-10-02 12:34:53 +0200

    DEL-A-5

--- Report-company.py
+++ Report-company.py
@@ -12,16 +12,16 @@ report += "\n" + "Company Name: "  + data["company"] + "\n"
 report += "\n" + "LAST UPDATED:" "\n" + data["last_updated"].replace("T"," ")  + "\n"
 
 #2 List all devices with warning
-report += "\n" "ATTENTION! Devices with status: Warning!" "\n"
-report += "--------------------------------------------------------------------------------------------" "\n"
+report += "############################################################################################"
+report += "\n" "ATTENTION! Devices with status: Warning!" "\n\n"
 for location in data["locations"]:
    for device in location["devices"]:
       if device["status"] != "online" and device["status"] != "offline":
          report += device["hostname"].ljust (12) + " | " + device["status"] + " | " + device["ip_address"].ljust (14) + " | " + device["type"].ljust (12) + " | " + location["site"] + "\n"
 
 # List all devices with Offline
-report += "\n" "ATTENTION! Devices with status: Offline!" "\n"
-report += "--------------------------------------------------------------------------------------------" "\n"
+report += "--------------------------------------------------------------------------------------------"
+report += "\n" "ATTENTION! Devices with status: Offline!" "\n\n"
 for location in data["locations"]:
    for device in location ["devices"]:
       if device["status"] != "online" and device["status"] != "warning":
@@ -29,8 +29,8 @@ for location in data["locations"]:
 
 #3. Total devices per type
 device_count = {}
-report += "\n" "---Number of devices---" "\n"
-report +="------------------------------------------------------------------------------------------" "\n"
+report +="------------------------------------------------------------------------------------------"
+report += "\n" "-Number of devices-" "\n\n"
 for location in data["locations"]:
    for device in location["devices"]:
         t = device["type"]
@@ -41,14 +41,35 @@ for t in device_count:
    report += t.ljust(13) + ": " + str(device_count[t]) + "\n"
 
 #4. Devices with less than 30 days uptime
-report += "\n" "---Devices with < 30 days uptime---" "\n"
-report +="------------------------------------------------------------------------------------------" "\n"
+report +="------------------------------------------------------------------------------------------"
+report += "\n" "-Devices with < 30 days uptime-" "\n\n"
 for location in data["locations"]:
    for device in location["devices"]:
       if device.get("uptime_days", 9999) < 30:
-         report += device["hostname"].ljust(13) + " " + str(device["uptime_days"]).ljust(3) + " " + "days" + "\n"
-      
-      
+         report += device["hostname"].ljust(13) + "|" + " " + str(device["uptime_days"]).ljust(3) + "|" + " " + "days" + "\n"
+
+#5. Calculate total port usage
+report += "------------------------------------------------------------------------------------------" 
+report += "\n" "-Switch port usage-"
+
+total_ports = 0
+used_ports = 0
+total_used = 0
+
+for location in data["locations"]:
+   for device in location["devices"]:
+      if device.get("type") == "Switch":
+         ports = device.get("ports")
+         if ports:
+            total_used += ports["used"]
+            total_ports += ports["total"]
+percent = round(total_used / total_ports * 100, 1)
+report += ("\n Total switch port usage: "
+           + str(total_used) + "/"
+           + str(total_ports) + " ("
+           + str(percent) + "%)\n" + "\n")
+       
+
 
 # loop through the location list 
 for location in data["locations"]:
