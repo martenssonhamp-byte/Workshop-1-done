32453ea81490ba154feb10c33e20fb3a52869305
martenssonhamp-byte <martensson.hamp@gmail.com>
2025-10-02 22:27:19 +0200

    DEL-C-Done

--- Report-company.py
+++ Report-company.py
@@ -6,13 +6,13 @@ data = json.load(open("network_devices.json","r",encoding = "utf-8"))
 
 # Create a variable that holds our whole text report
 report = ""
-
+intro_report = ""
 #1 Last updated report & company name
-report += "\n" + "Company Name: "  + data["company"] + "\n"
-report += "\n" + "LAST UPDATED:" "\n" + data["last_updated"].replace("T"," ")  + "\n"
+intro_report += "\n" + "Company Name: "  + data["company"] + "\n"
+intro_report += "\n" + "LAST UPDATED:" "\n" + data["last_updated"].replace("T"," ")  + "\n"
 
 #2 List all devices with warning
-report += "############################################################################################"
+report += "\n############################################################################################"
 report += "\n" "ATTENTION! Devices with status: Warning!" "\n\n"
 for location in data["locations"]:
    for device in location["devices"]:
@@ -50,19 +50,32 @@ for location in data["locations"]:
 
 #5. Calculate total port usage
 report += "------------------------------------------------------------------------------------------" 
-report += "\n" "-Switch port usage-"
-
+report += "\n" "-Switch port usage-\n"
+report += "   Site ".ljust(12) + "Switches " + " Used/total " + " Percent " + "\n"
 total_ports = 0
 used_ports = 0
 total_used = 0
 # Total port usage
 for location in data["locations"]:
-   for device in location["devices"]:
-      if device.get("type") == "Switch":
-         ports = device.get("ports")
-         if ports:
-            total_used += ports["used"]
-            total_ports += ports["total"]
+    port_count = 0
+    used_count = 0
+    switch_count = 0
+    for device in location["devices"]:
+        if device.get("type") == "Switch":
+            switch_count += 1
+            ports = device.get("ports")
+            if ports:
+                used_count += ports["used"]
+                port_count += ports["total"]
+    total_used += used_count
+    total_ports += port_count
+    percent_site = round(used_count / port_count * 100, 1)
+    report += location["site"].ljust(15) + " " + str(switch_count).ljust(5) + " " + str(used_count).ljust(3) + "/" + str(port_count).ljust(8) + str(percent_site) + "%"
+    if percent_site > 80:
+        report += " ⚠"
+        if percent_site > 90:
+           report += " CRITICAL!"
+    report += "\n"
 percent = round(total_used / total_ports * 100, 1)
 report += ("\n Total switch port usage: "
            + str(total_used) + "/"
@@ -79,7 +92,7 @@ for location in data["locations"]:
          if ports:
             used = ports["used"]
             total = ports["total"]
-            percent = round(used / total * 100, 1) if total > 0 else 0
+            percent = round(used / total * 100, 1)
             if percent >= 80:
                 warning = "CAUTION!"
 
@@ -112,6 +125,7 @@ for i, vlan in enumerate(sorted(vlans_set)): # i = index, row 106 - 109 is all a
 report += "\n" "------------------------------------------------------------------------------------------" 
 report += "\n\n" + "-Overview-" + "\n"
 report += "Site:".ljust(15) + " Devices  " + "Online  " + "Offline  " + "\n"
+total_offline_count = 0
 for location in data["locations"]:
     num_devices = 0
     offline_count = 0
@@ -122,12 +136,38 @@ for location in data["locations"]:
             offline_count += 1
         elif device["status"] == "online":
            online_count += 1    
-
+    total_offline_count += offline_count
     report += location["site"].ljust(14) + " | " + str(num_devices).rjust(3) + " " + str(offline_count).rjust(7) + " " + str(online_count).rjust(8) + "\n"
       
       
+#Summary
+exec_report = ""
+exec_report += "############################################################################################"
+exec_report += "\n                            -----EXECUTIVE SUMMARY-----\n"
+warning_count = 0
+low_uptime_count = 0
+high_port_count = 0
+for location in data ["locations"]:
+    for device in location["devices"]:
+        if device["status"] == "warning":
+            warning_count += 1
+        if device.get("uptime_days", 9999) < 30:
+            low_uptime_count += 1    
+        if device.get("type") == "Switch":
+            ports = device.get("ports")
+            if ports:
+                used = ports["used"]
+                total = ports["total"]
+                percent = round(used / total * 100, 1)
+                if percent > 80:
+                    high_port_count += 1
 
-
+exec_report += "⚠ CRITICAL! " + str(total_offline_count) + " devices offline" + "\n"
+exec_report += "⚠ WARNINGS! " + str(warning_count) + " devices with warning status" + "\n"
+exec_report += "⚠ " + str(low_uptime_count) + " devices with low uptime < 30 days, could indicate instability!" + "\n" 
+exec_report += "⚠ " + str(high_port_count) + " switches with high port usage > 80%! " + "\n"
 # write the report to text file
 with open('report.txt', 'w', encoding='utf-8') as f:
+    f.write(intro_report)
+    f.write(exec_report)
     f.write(report)
