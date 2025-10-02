4fea05cde2787a181d0798bab3ae7452074e003d
martenssonhamp-byte <martensson.hamp@gmail.com>
2025-10-02 09:32:48 +0200

    added the start help we got from teacher

---
+++ Report-company.py
@@ -0,0 +1,22 @@
+
+#Import the JSON file
+import json
+
+# Read json from products.json to the variable data
+data = json.load(open("network_devices.json","r",encoding = "utf-8"))
+
+# Create a variable that holds our whole text report
+report = ""
+
+# loop through the location list 
+for location in data["locations"]:
+    # add the site/'name' of the location to the report
+    report += "\n" + location["site"] + "\n"
+    # add a list of the host names of the devices 
+    # on the location to the report
+    for device in location["devices"]:
+      report += "  " + device["hostname"] + "\n"
+
+# write the report to text file
+with open('report.txt', 'w', encoding='utf-8') as f:
+    f.write(report)
